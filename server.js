bc1bf7fdd8c400435bd06e920eac20dcf5dc45d2
Kenrick <krchan@gmail.com>
2015-03-29 17:14:00 -0700

    added a procfile

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res)
 {
 	res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
