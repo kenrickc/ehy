86b2186d73a02286260cb93c98f70aff9dd2d1e6
Kenrick <krchan@gmail.com>
2015-03-29 20:10:02 -0700

    added pg

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+	var router = express.Router();
+
+	router.route('/users')
+		.post(function(req, res) {
+
+		})
+		.get(function(req, res){
+			pg.connect(process.env.DATABASE_URL, function(err, client, done){
+				client.query('SELECT * FROM users', function(err, result){
+					done();
+					if(err)
+						{ console.error(err); response.send("Error " + err); }
+					else
+						{ response.send(result.rows); }
+				});
+			});
+		})
+}
